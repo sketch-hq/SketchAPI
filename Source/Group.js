528d19a6604868283dc97d58c30b89a6a85b3bd3
Sam Deane <sam@elegantchaos.com>
2016-07-12 17:43:41 +0100

    Updated comments.

--- Source/Group.js
+++ Source/Group.js
@@ -9,10 +9,22 @@
 
 import { Layer } from './Layer.js'
 
+/**
+  Represents a group of layers.
+ */
+
 export class Group extends Layer {
     constructor(group, document) {
       super(group, document)
     }
+
+    /**
+        Is this an group?
+
+        All Layer objects respond to this method, but only Groups or things that inherit from groups return true.
+
+        @return true for instances of Group, false for any other layer type.
+    */
 
     is_group() {
       return true;
