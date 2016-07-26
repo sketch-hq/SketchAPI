39e4d6c9a27867e908971a762526a977974265c9
Sam Deane <sam@elegantchaos.com>
2016-07-26 18:26:58 +0100

    Experimenting with @test comments.

--- Source/Application.js
+++ Source/Application.js
@@ -19,156 +19,156 @@ import { Layer } from './Layer.js'
 import { Selection } from './Selection.js'
 
 /**
-    Gives you access to Sketch, and provides access to:
-    - the document model and the layer tree
-    - metadata abound sketch itself
-    - utilities for interacting with the user
-    - access to the running plugin, it's resources and settings
-*/
+ Gives you access to Sketch, and provides access to:
+ - the document model and the layer tree
+ - metadata abound sketch itself
+ - utilities for interacting with the user
+ - access to the running plugin, it's resources and settings
+ */
 
 export class Application extends WrappedObject {
 
     /**
-        Construct a new Application object.
-        An instance of this class will be passed back to you when you
-        initialise the API, so you generally shouldn't need to make new ones.
+     Construct a new Application object.
+     An instance of this class will be passed back to you when you
+     initialise the API, so you generally shouldn't need to make new ones.
 
-        @param context The context dictionary passed to the script when it was invoked.
-        @return A new Application object.
-    */
+     @param context The context dictionary passed to the script when it was invoked.
+     @return A new Application object.
+     */
 
     constructor(context) {
-      super(context)
+        super(context)
 
-      /**
-        Metadata about this version of Sketch.
-        @type {dictionary}
-      */
-      this._metadata = MSApplicationMetadata.metadata()
+        /**
+         Metadata about this version of Sketch.
+         @type {dictionary}
+         */
+        this._metadata = MSApplicationMetadata.metadata()
 
 
-      /**
-        This is a slightly clumsy way to pass related classes into Layer
-        without setting up a circular dependency between classes (eg Layer imports Artboard which imports Layer...)
-        there has to be a better way to do this...
+        /**
+         This is a slightly clumsy way to pass related classes into Layer
+         without setting up a circular dependency between classes (eg Layer imports Artboard which imports Layer...)
+         there has to be a better way to do this...
 
-        @type {dictionary}
-      */
+         @type {dictionary}
+         */
 
-      this.factory = {
-        "Group" : Group,
-        "Page" : Page,
-        "Artboard" : Artboard,
-        "Shape" : Shape,
-        "Image" : Image,
-        "Text" : Text
-      }
+        this.factory = {
+            "Group" : Group,
+            "Page" : Page,
+            "Artboard" : Artboard,
+            "Shape" : Shape,
+            "Image" : Image,
+            "Text" : Text
+        }
     }
 
     /**
-        The version of this API.
+     The version of this API.
 
-        @return A version string.
-    */
+     @return A version string.
+     */
 
     get api_version() {
-      return "1.1"
+        return "1.1"
     }
 
     /**
-        The context that the API was created in.
-        This is the traditional context argument that is
-        passed to all plugin scripts when they are run.
+     The context that the API was created in.
+     This is the traditional context argument that is
+     passed to all plugin scripts when they are run.
 
-        In general you should use the API to access Sketch
-        rather than interacting with the context directly.
+     In general you should use the API to access Sketch
+     rather than interacting with the context directly.
 
-        @return A context dictionary.
-    */
+     @return A context dictionary.
+     */
 
     get context() {
-      return this._object
+        return this._object
     }
 
 
     /**
-        The version of Sketch that is running.
+     The version of Sketch that is running.
 
-        @return The version as a string, eg "3.6".
-    */
+     @return The version as a string, eg "3.6".
+     */
 
     get version() {
-      return this._metadata['appVersion']
+        return this._metadata['appVersion']
     }
 
     /**
-        The exact build of Sketch that is running.
+     The exact build of Sketch that is running.
 
-        @return The build number as a string, eg "15352".
-    */
+     @return The build number as a string, eg "15352".
+     */
 
     get build() {
-      return this._metadata['build']
+        return this._metadata['build']
     }
 
     /**
-        Returns the full version of Sketch that is running
+     Returns the full version of Sketch that is running
 
-        @return {string} Version and build number as a string, eg "3.6 (15352)".
-    */
+     @return {string} Version and build number as a string, eg "3.6 (15352)".
+     */
 
     get full_version() {
-      return this.version + " (" + this.build + ")"
+        return this.version + " (" + this.build + ")"
     }
 
     /**
-        Return the value of a global setting for a given key.
-        @param key The setting to look up.
-        @return The setting value.
+     Return the value of a global setting for a given key.
+     @param key The setting to look up.
+     @return The setting value.
 
-        This is equivalent to reading a setting for the currently
-        running version of Sketch using the `defaults` command line tool,
-        eg: defaults read com.bohemiancoding.sketch3 <key>
-    */
+     This is equivalent to reading a setting for the currently
+     running version of Sketch using the `defaults` command line tool,
+     eg: defaults read com.bohemiancoding.sketch3 <key>
+     */
 
     settingForKey(key) {
-      return NSUserDefaults.standardUserDefaults()._objectForKey_(key);
+        return NSUserDefaults.standardUserDefaults()._objectForKey_(key);
     }
 
     /**
-        Set the value of a global setting for a given key.
+     Set the value of a global setting for a given key.
 
-        @param key The setting to set.
-        @param value The value to set it to.
+     @param key The setting to set.
+     @param value The value to set it to.
 
-        This is equivalent to writing a setting for the currently
-        running version of Sketch using the `defaults` command line tool,
-        eg: defaults write com.bohemiancoding.sketch3 <key> <value>
-    */
+     This is equivalent to writing a setting for the currently
+     running version of Sketch using the `defaults` command line tool,
+     eg: defaults write com.bohemiancoding.sketch3 <key> <value>
+     */
 
     setSettingForKey(key, value) {
-      NSUserDefaults.standardUserDefaults().setObject_forKey_(value, key)
+        NSUserDefaults.standardUserDefaults().setObject_forKey_(value, key)
     }
 
     /**
-        Return a file URL pointing to a named resource in the plugin's Resources/
-        folder.
+     Return a file URL pointing to a named resource in the plugin's Resources/
+     folder.
 
-        @param name The resource file name, including any file extension.
-        @return The resource location, in the form "file://path/to/resource".
-    */
+     @param name The resource file name, including any file extension.
+     @return The resource location, in the form "file://path/to/resource".
+     */
 
     resourceNamed(name) {
-      return this._object.plugin.urlForResourceNamed_(name)
+        return this._object.plugin.urlForResourceNamed_(name)
     }
 
     /**
-        Shows a simple input sheet which displays a message, and asks for a single string
-        input.
-        @param msg The prompt message to show.
-        @param initial The initial value of the input string.
-        @return The string that the user input.
-    */
+     Shows a simple input sheet which displays a message, and asks for a single string
+     input.
+     @param msg The prompt message to show.
+     @param initial The initial value of the input string.
+     @return The string that the user input.
+     */
 
     getStringFromUser(msg, initial) {
         var panel = MSModalInputSheet.alloc().init()
@@ -177,162 +177,166 @@ export class Application extends WrappedObject {
     }
 
     /**
-        Shows an input sheet which displays a popup with a series of options,
-        from which the user is asked to choose.
+     Shows an input sheet which displays a popup with a series of options,
+     from which the user is asked to choose.
 
-        @param msg The prompt message to show.
-        @param items A list of option items.
-        @param selectedItemIndex The index of the item to select initially.
-        @return An array with two items: [responseCode, selection].
+     @param msg The prompt message to show.
+     @param items A list of option items.
+     @param selectedItemIndex The index of the item to select initially.
+     @return An array with two items: [responseCode, selection].
 
-        The result consists of a response code and a selection. The code will be
-        one of NSAlertFirstButtonReturn or NSAlertSecondButtonReturn.
-        The selection will be the integer index of the selected item.
-    */
+     The result consists of a response code and a selection. The code will be
+     one of NSAlertFirstButtonReturn or NSAlertSecondButtonReturn.
+     The selection will be the integer index of the selected item.
+     */
 
     getSelectionFromUser(msg, items, selectedItemIndex) {
-      selectedItemIndex = selectedItemIndex || 0
+        selectedItemIndex = selectedItemIndex || 0
 
-      var accessory = NSComboBox.alloc().initWithFrame(NSMakeRect(0,0,200,25))
-      accessory.addItemsWithObjectValues(items)
-      accessory.selectItemAtIndex(selectedItemIndex)
+        var accessory = NSComboBox.alloc().initWithFrame(NSMakeRect(0,0,200,25))
+        accessory.addItemsWithObjectValues(items)
+        accessory.selectItemAtIndex(selectedItemIndex)
 
-      var alert = NSAlert.alloc().init()
-      alert.setMessageText(msg)
-      alert.addButtonWithTitle('OK')
-      alert.addButtonWithTitle('Cancel')
-      alert.setAccessoryView(accessory)
+        var alert = NSAlert.alloc().init()
+        alert.setMessageText(msg)
+        alert.addButtonWithTitle('OK')
+        alert.addButtonWithTitle('Cancel')
+        alert.setAccessoryView(accessory)
 
-      var responseCode = alert.runModal()
-      var sel = accessory.indexOfSelectedItem()
+        var responseCode = alert.runModal()
+        var sel = accessory.indexOfSelectedItem()
 
-      return [responseCode, sel]
+        return [responseCode, sel]
     }
 
     /**
-        Output a message to the log console.
+     Output a message to the log console.
 
-        @param {string} message The message to output.
-    */
+     @param {string} message The message to output.
+     */
 
     log(message) {
-      print(message)
+        print(message)
     }
 
     /**
-        Assert that a given condition is true.
-        If the condition is false, throws an exception.
+     Assert that a given condition is true.
+     If the condition is false, throws an exception.
 
-        @param condition An expression that is expected to evaluate to true if everything is ok.
-    */
+     @param condition An expression that is expected to evaluate to true if everything is ok.
+     */
 
     assert(condition) {
-      if (!condition) {
-          throw "Assert failed!"
-      }
+        if (!condition) {
+            throw "Assert failed!"
+        }
     }
 
     /**
-        The selected document.
+     The selected document.
 
-        If the user invoked the script explicitly (for example by selecting a menu item),
-        this will be the document that they were working in at the time - ie the frontmost one.
-        If the script was invoked as an action handler, this will be the document that the action
-        occurred in.
+     If the user invoked the script explicitly (for example by selecting a menu item),
+     this will be the document that they were working in at the time - ie the frontmost one.
+     If the script was invoked as an action handler, this will be the document that the action
+     occurred in.
 
-        @return A Document object.
-    */
+     @return A Document object.
+     */
 
     get selectedDocument() {
-      return new Document(this._object.document, this)
+        return new Document(this._object.document, this)
     }
 
 
     /**
-        Create a new document and bring it to the front.
-        @return The new document.
-    */
+     Create a new document and bring it to the front.
+     @return The new document.
+     */
 
     newDocument() {
-      var app = NSDocumentController.sharedDocumentController()
-      app.newDocument_(this)
-      return new Document(app.currentDocument(), this)
+        var app = NSDocumentController.sharedDocumentController()
+        app.newDocument_(this)
+        return new Document(app.currentDocument(), this)
 
     }
 
     /**
-        Show a small, temporary, message to the user.
-        The message appears at the bottom of the selected document,
-        and is visible for a short period of time. It should consist of a single
-        line of text.
+     Show a small, temporary, message to the user.
+     The message appears at the bottom of the selected document,
+     and is visible for a short period of time. It should consist of a single
+     line of text.
 
-        @param {string} message The message to show.
-    */
+     @param {string} message The message to show.
+     */
 
     message(message) {
-      this._object.document.showMessage(message)
+        this._object.document.showMessage(message)
     }
 
     /**
-        Show an alert with a custom title and message.
+     Show an alert with a custom title and message.
 
-        @param {string} title The title of the alert.
-        @param {string} message The text of the message.
+     @param {string} title The title of the alert.
+     @param {string} message The text of the message.
 
-        The alert is modal, so it will stay around until the user dismisses it
-        by pressing the OK button.
-    */
+     The alert is modal, so it will stay around until the user dismisses it
+     by pressing the OK button.
+     */
 
     alert(title, message) {
-      var app = NSApplication.sharedApplication()
-      app.displayDialog_withTitle(title, message)
+        var app = NSApplication.sharedApplication()
+        app.displayDialog_withTitle(title, message)
     }
 
     /**
-        Return a new Rectangle object for a given x,y, width and height.
+     Return a new Rectangle object for a given x,y, width and height.
 
-        @param {number} x The x coordinate of the top-left corner of the rectangle.
-        @param {number} y The y coordinate of the top-left corner of the rectangle.
-        @param {number} width The width of the rectangle.
-        @param {number} height The height of the rectangle.
-        @return The new Rectangle object.
-    */
+     @param {number} x The x coordinate of the top-left corner of the rectangle.
+     @param {number} y The y coordinate of the top-left corner of the rectangle.
+     @param {number} width The width of the rectangle.
+     @param {number} height The height of the rectangle.
+     @return The new Rectangle object.
+     */
 
     rectangle(x, y, width, height) {
-      return new Rectangle(x, y, width, height)
+        return new Rectangle(x, y, width, height)
     }
 
     /**
-        Return a list of tests to run for this class.
-        
-        We could do some fancy introspection here to derive the tests from
-        the class, but for now we're opting for the simple approach.
-        
-        @return {dictionary} A dictionary containing the tests to run. Each key is the name of a test, each value is a function which takes a Tester instance.
-    */
+     Return a list of tests to run for this class.
+
+     We could do some fancy introspection here to derive the tests from
+     the class, but for now we're opting for the simple approach.
+
+     @return {dictionary} A dictionary containing the tests to run. Each key is the name of a test, each value is a function which takes a Tester instance.
+     */
 
     static tests() {
-      return {
-          "tests" : {
-            "test api version" : function(tester) {
-              tester.assertEqual(tester.application.api_version, "1.1");
-            },
-            "test app version" : function(tester) {
-              tester.assertEqual(tester.application.version, "1.0");
+        return {
+            /** @test {Application} */
+            "tests" : {
+                /** @test {Application#api_version} */
+                "test api version" : function(tester) {
+                    tester.assertEqual(tester.application.api_version, "1.1");
+                },
+
+                /** @test {Application#version} */
+                "test app version" : function(tester) {
+                    tester.assertEqual(tester.application.version, "1.0");
+                }
             }
-          }
-      };
+        };
     }
 
     /**
-        Run all of our internal unit tests.
-        Returns a dictionary indicating how many tests ran, passed, failed, and crashed,
-        and a list of more detailed information for each failure.
+     Run all of our internal unit tests.
+     Returns a dictionary indicating how many tests ran, passed, failed, and crashed,
+     and a list of more detailed information for each failure.
 
-        At some point we may switch to using Mocha or some other test framework, but for
-        now we want to be able to invoke the tests from the Sketch side or from a plugin
-        command, so it's simpler to use a simple test framework of our own devising.
-    */
+     At some point we may switch to using Mocha or some other test framework, but for
+     now we want to be able to invoke the tests from the Sketch side or from a plugin
+     command, so it's simpler to use a simple test framework of our own devising.
+     */
 
     runUnitTests() {
         var tests = {
@@ -351,9 +355,9 @@ export class Application extends WrappedObject {
                 "WrappedObject" : WrappedObject.tests()
             }
         }
-
-      var tester = new Tester(this);
-      return tester.runUnitTests(tests);
+        
+        var tester = new Tester(this);
+        return tester.runUnitTests(tests);
     }
-
+    
 }
