1c946fdbcb10266459cf0d30e1823e0a575d0a12
Sam Deane <sam@elegantchaos.com>
2016-09-06 10:58:26 +0100

    Added pageRectToLocalRect

--- Source/Group.js
+++ Source/Group.js
@@ -6,7 +6,7 @@
 // ********************************
 
 import { Layer } from './Layer.js'
-
+import { Rectangle } from './Rectangle.js'
 /**
   Represents a group of layers.
  */
@@ -41,6 +41,16 @@ export class Group extends Layer {
 
      @return {dictionary} A dictionary containing the tests to run. Each key is the name of a test, each value is a function which takes a Tester instance.
      */
+
+     /**
+       Convert a rectangle in page coordinates to one relative to this container's coordinates.
+      */
+
+     pageRectToLocalRect(rect) {
+         var origin = this._object.convertPoint_fromLayer_(NSMakePoint(rect.x, rect.y), null)
+         return new Rectangle(origin.x, origin.y, rect.width, rect.height)
+     }
+
 
     static tests() {
         return {
