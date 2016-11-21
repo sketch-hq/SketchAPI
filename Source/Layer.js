07db9696a6647030b15fd7f8282e94d717962046
David Keegan <git@davidkeegan.com>
2016-11-20 19:14:59 -0800

    Add get parentArtboard function to Layer

--- Source/Layer.js
+++ Source/Layer.js
@@ -205,6 +205,16 @@ export class Layer extends WrappedObject {
   }
 
   /**
+  Return the parent artboard of this layer.
+
+  @return {Artboard} The parent artboard of this layer.
+  */
+
+  get parentArtboard() {
+    return this._document.wrapObject(this._object.parentArtboard())
+  }  
+
+  /**
   Return a list of tests to run for this class.
 
   @return {dictionary} A dictionary containing the tests to run. Each key is the name of a test, each value is a function which takes a Tester instance.
