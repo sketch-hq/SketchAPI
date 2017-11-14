ed94e3b9dee42274f701e780f4fba672b51454ee
pravdomil <pravdomil.toman@gmail.com>
2017-11-14 19:36:52 +0100

    example plugin

---
+++ HelloTypeScript.sketchplugin/Contents/Sketch/hellots.js
@@ -0,0 +1,4 @@
+function onRun(context) {
+    const app = context.api();
+    app.alert("Hello TypeScript!", "");
+}
---
+++ HelloTypeScript.sketchplugin/Contents/Sketch/hellots.ts
@@ -0,0 +1,4 @@
+function onRun(context: SketchContext) {
+    const app = context.api();
+    app.alert("Hello TypeScript!", "");
+}
