2da167454c43bd821f18a33577a46bd55c08537d
mathieudutour <mathieu@dutour.me>
2017-12-06 11:39:13 +0000

    fix copy tests script

--- scripts/copy-tests.js
+++ scripts/copy-tests.js
@@ -3,20 +3,21 @@ const fs = require('fs')
 const path = require('path')
 
 // heuristic to know if we are inside the Sketch repo
-const IS_BC_BUILD = /\/Modules\/SketchAPI$/.test(__dirname)
+const IS_BC_BUILD = /\/Modules\/SketchAPI/.test(__dirname)
 
 if (IS_BC_BUILD) {
   fs
     .createReadStream(
       path.join(
         __dirname,
-        './.test-runner.sketchplugin/Contents/Sketch/SketchAPI.tests.js'
+        '../.test-runner.sketchplugin/Contents/Sketch/SketchAPI.tests.js'
       )
     )
     .pipe(
       fs.createWriteStream(
         path.join(
-          '../SketchPluginManager/Tests/Data/PluginScriptTests/SketchAPI.tests.js'
+          __dirname,
+          '../../SketchPluginManager/Tests/Data/PluginScriptTests/SketchAPI.tests.js'
         )
       )
     )
