5df54db903922bec3a6abf6a09dccfd57359eddd
mathieudutour <mathieu@dutour.me>
2017-12-22 11:27:09 +0100

    make the tests for symbol use the MSDocData

--- Source/components/__tests__/Symbol.test.js
+++ Source/components/__tests__/Symbol.test.js
@@ -1,7 +1,7 @@
 /* globals expect, test */
 /* eslint-disable no-param-reassign */
 
-import { SymbolMaster, Text, Artboard, Document } from '../../'
+import { SymbolMaster, Text, Artboard } from '../../'
 
 function createSymbolMaster(document) {
   const artboard = new Artboard({
@@ -25,6 +25,9 @@ test('should create a symbol master from an artboard', (context, document) => {
   // build the symbol master
   const { master } = createSymbolMaster(document)
   expect(master.type).toBe('SymbolMaster')
+
+  expect(document.getLayerNamed).toBeDefined()
+  expect(document.getSymbolMasterWithID(master.symbolId)).toEqual(master)
 })
 
 test('should create a symbol instance from a master', (context, document) => {
@@ -36,17 +39,17 @@ test('should create a symbol instance from a master', (context, document) => {
   // create an instance
   const instance = master.createNewInstance()
   expect(instance.type).toBe('SymbolInstance')
+  expect(instance.master).toBe(null)
   // by default, it's not anywhere in the document
   expect(master.getAllInstances()).toEqual([])
 
   // add the instance to the page
   document.selectedPage.layers = document.selectedPage.layers.concat(instance)
-  expect(document.selectedPage.layers).toEqual([instance])
+  expect(master.getAllInstances()).toEqual([instance])
+  expect(instance.master).toEqual(master)
 })
 
-test('should have overrides', context => {
-  const document = Document.fromNative(context.document)
-
+test('should have overrides', (context, document) => {
   const { master, text } = createSymbolMaster(document)
   const instance = master.createNewInstance()
   document.selectedPage.layers = document.selectedPage.layers.concat(instance)
@@ -63,9 +66,7 @@ test('should have overrides', context => {
   })
 })
 
-test('should be able to set overrides', context => {
-  const document = Document.fromNative(context.document)
-
+test('should be able to set overrides', (context, document) => {
   const { master, text } = createSymbolMaster(document)
   const instance = master.createNewInstance()
   document.selectedPage.layers = document.selectedPage.layers.concat(instance)
@@ -75,7 +76,7 @@ test('should be able to set overrides', context => {
   override.value = 'overridden'
 
   expect(instance.overrides.length).toBe(1)
-  expect(instance.overrides[0]).toEqual({
+  expect(instance.overrides[0].toJSON()).toEqual({
     id: `${text.id}_stringValue`,
     path: text.id,
     property: 'stringValue',
