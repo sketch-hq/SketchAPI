2921264e1085397e25814194165ec7fe9872dd75
mathieudutour <mathieu@dutour.me>
2018-01-08 13:42:49 +0100

    add test for nested override

--- Source/components/__tests__/Symbol.test.js
+++ Source/components/__tests__/Symbol.test.js
@@ -135,3 +135,39 @@ test(
     })
   }
 )
+
+test('should change a nested symbol', (context, document) => {
+  // build the first symbol master
+  const { master: nestedMaster } = createSymbolMaster(document)
+  const { master: nestedMaster2 } = createSymbolMaster(document)
+
+  const artboard = new Artboard({
+    name: 'Test2',
+    parent: document.selectedPage,
+  })
+  const text2 = new Text({
+    text: 'Test value 2',
+  })
+  const nestedInstance = nestedMaster.createNewInstance()
+  artboard.layers = [nestedInstance, text2]
+
+  const master = SymbolMaster.fromArtboard(artboard)
+
+  const instance = master.createNewInstance()
+
+  // add the instance to the page
+  document.selectedPage.layers = document.selectedPage.layers.concat(instance)
+  expect(instance.overrides.length).toBe(3)
+
+  const override = instance.overrides[0]
+  override.value = nestedMaster2.symbolId
+
+  expect(instance.overrides[0].toJSON()).toEqual({
+    id: `${nestedInstance.id}_symbolID`,
+    path: nestedInstance.id,
+    property: 'symbolID',
+    symbolOverride: true,
+    value: nestedMaster2.symbolId,
+    isDefault: false,
+  })
+})
