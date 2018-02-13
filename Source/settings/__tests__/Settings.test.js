04f88fc68c478df5236231e8829a1685f0892e35
mathieudutour <mathieu@dutour.me>
2018-02-13 16:22:27 +0100

    fix settings tests

--- Source/settings/__tests__/Settings.test.js
+++ Source/settings/__tests__/Settings.test.js
@@ -2,56 +2,44 @@
 
 import { settingForKey, setSettingForKey } from '../Settings'
 
-function madeupContextForTempScript(context) {
-  if (!context.plugin) {
-    // eslint-disable-next-line
-    context.plugin = {
+function madeupPluginIdentifierForTempScript() {
+  if (!__command.pluginBundle()) {
+    __command.pluginBundle = {
       identifier() {
         return 'temp.script'
       },
     }
   }
-
-  return context
 }
 
-test('non existing settings should return null', context => {
-  expect(
-    settingForKey(madeupContextForTempScript(context), 'non-existing-key')
-  ).toBe(null)
+test('non existing settings should return null', () => {
+  madeupPluginIdentifierForTempScript()
+  expect(settingForKey('non-existing-key')).toBe(null)
 })
 
-test('should set a boolean', context => {
-  setSettingForKey(madeupContextForTempScript(context), 'false-key', false)
-  expect(settingForKey(madeupContextForTempScript(context), 'false-key')).toBe(
-    false
-  )
+test('should set a boolean', () => {
+  madeupPluginIdentifierForTempScript()
+  setSettingForKey('false-key', false)
+  expect(settingForKey('false-key')).toBe(false)
 
-  setSettingForKey(madeupContextForTempScript(context), 'true-key', true)
-  expect(settingForKey(madeupContextForTempScript(context), 'true-key')).toBe(
-    true
-  )
+  setSettingForKey('true-key', true)
+  expect(settingForKey('true-key')).toBe(true)
 })
 
-test('should set a string', context => {
-  setSettingForKey(madeupContextForTempScript(context), 'string-key', 'test')
-  expect(settingForKey(madeupContextForTempScript(context), 'string-key')).toBe(
-    'test'
-  )
+test('should set a string', () => {
+  madeupPluginIdentifierForTempScript()
+  setSettingForKey('string-key', 'test')
+  expect(settingForKey('string-key')).toBe('test')
 })
 
-test('should set undefined', context => {
-  setSettingForKey(
-    madeupContextForTempScript(context),
-    'undefined-key',
-    undefined
-  )
-  expect(
-    settingForKey(madeupContextForTempScript(context), 'undefined-key')
-  ).toBe(undefined)
+test('should set undefined', () => {
+  madeupPluginIdentifierForTempScript()
+  setSettingForKey('undefined-key', undefined)
+  expect(settingForKey('undefined-key')).toBe(undefined)
 })
 
-test('should set object', context => {
-  setSettingForKey(context, 'object-key', { a: 1 })
-  expect(settingForKey(context, 'object-key')).toEqual({ a: 1 })
+test('should set object', () => {
+  madeupPluginIdentifierForTempScript()
+  setSettingForKey('object-key', { a: 1 })
+  expect(settingForKey('object-key')).toEqual({ a: 1 })
 })
