b47030e70344b463258cbe0a13032002ac78e370
mathieudutour <mathieu@dutour.me>
2018-03-29 16:23:35 +0200

    fix lint

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,20 +4,18 @@ import { danger, warn } from 'danger'
 const touchedFiles = danger.git.modified_files.concat(danger.git.created_files)
 
 const LIB_REGEX = /^Source\/.*\.js$/
-const TEST_REGEX = /^Source\/.*\/__tests__\/.*\.test\.js?$/
-const DOCS_REGEX = /^docs\/api\/.*\.md$/
 
 const libraryChanges = touchedFiles.filter(
-  path => LIB_REGEX.test(path) && path.indexOf('__tests__') === -1
+  filePath => LIB_REGEX.test(filePath) && filePath.indexOf('__tests__') === -1
 )
 
 function matchingTest(dir: string, name: string) {
-  const testPath = path.join(dir, '__tests__', name + '.js')
+  const testPath = path.join(dir, '__tests__', `${name}.js`)
   return touchedFiles.find(f => f === testPath)
 }
 
 function matchingDoc(dir: string, name: string) {
-  const docPath = path.join(dir.split('Source')[0], 'docs', 'api', name + '.md')
+  const docPath = path.join(dir.split('Source')[0], 'docs', 'api', `${name}.md`)
   return touchedFiles.find(f => f === docPath)
 }
 
@@ -25,7 +23,9 @@ function matchingDoc(dir: string, name: string) {
  * CHECK FOR CHANGELOG UPDATE
  */
 
-const hasCHANGELOGChanges = touchedFiles.some(path => path === 'CHANGELOG.json')
+const hasCHANGELOGChanges = touchedFiles.some(
+  filePath => filePath === 'CHANGELOG.json'
+)
 const hasLibraryChanges = libraryChanges.length > 0
 
 if (hasLibraryChanges && !hasCHANGELOGChanges) {
@@ -35,8 +35,12 @@ if (hasLibraryChanges && !hasCHANGELOGChanges) {
 /**
  * CHECK FOR PACKAGE.JSON UPDATE
  */
-const packageChanged = touchedFiles.some(path => path === 'package.json')
-const lockfileChanged = touchedFiles.some(path => path === 'package-lock.json')
+const packageChanged = touchedFiles.some(
+  filePath => filePath === 'package.json'
+)
+const lockfileChanged = touchedFiles.some(
+  filePath => filePath === 'package-lock.json'
+)
 
 if (packageChanged && !lockfileChanged) {
   const message =
@@ -46,10 +50,10 @@ if (packageChanged && !lockfileChanged) {
 }
 
 const corePackageChanged = touchedFiles.some(
-  path => path === 'core-modules/package.json'
+  filePath => filePath === 'core-modules/package.json'
 )
 const coreLockfileChanged = touchedFiles.some(
-  path => path === 'core-modules/package-lock.json'
+  filePath => filePath === 'core-modules/package-lock.json'
 )
 
 if (corePackageChanged && !coreLockfileChanged) {
@@ -64,23 +68,22 @@ if (corePackageChanged && !coreLockfileChanged) {
  */
 
 const ignoredFilesForTests = [
-  'async/index.js',
-  'data-supplier/index.js',
-  'dom/index.js',
-  'settings/index.js',
-  'ui/index.js',
+  'Source/async/index.js',
+  'Source/data-supplier/index.js',
+  'Source/dom/index.js',
+  'Source/settings/index.js',
+  'Source/ui/index.js',
 ]
 
 // Warn if there are library changes, but not tests
 libraryChanges.forEach(change => {
-  const fileName = change.split('Source')[1]
-  if (ignoredFilesForTests.indexOf(fileName) >= 0) {
+  if (ignoredFilesForTests.indexOf(change) >= 0) {
     return
   }
   const { dir, name } = path.parse(change)
   if (!matchingTest(dir, name)) {
     warn(
-      `\`${fileName}\` changed, but not its tests. That's OK as long as you're refactoring.`
+      `\`${change}\` changed, but not its tests. That's OK as long as you're refactoring.`
     )
   }
 })
@@ -90,28 +93,27 @@ libraryChanges.forEach(change => {
  */
 
 const ignoredFilesForDocs = [
-  'async/index.js',
-  'data-supplier/index.js',
-  'dom/enums.js',
-  'dom/Factory.js',
-  'dom/index.js',
-  'dom/utils.js',
-  'dom/wrapNativeObject.js',
-  'dom/WrappedObject.js',
-  'settings/index.js',
-  'ui/index.js',
+  'Source/async/index.js',
+  'Source/data-supplier/index.js',
+  'Source/dom/enums.js',
+  'Source/dom/Factory.js',
+  'Source/dom/index.js',
+  'Source/dom/utils.js',
+  'Source/dom/wrapNativeObject.js',
+  'Source/dom/WrappedObject.js',
+  'Source/settings/index.js',
+  'Source/ui/index.js',
 ]
 
 // Warn if there are library changes, but not docs
 libraryChanges.forEach(change => {
-  const fileName = change.split('Source')[1]
-  if (ignoredFilesForDocs.indexOf(fileName) >= 0) {
+  if (ignoredFilesForDocs.indexOf(change) >= 0) {
     return
   }
   const { dir, name } = path.parse(change)
   if (!matchingDoc(dir, name)) {
     warn(
-      `\`${fileName}\` changed, but not its doc. That's OK as long as you're refactoring.`
+      `\`${change}\` changed, but not its doc. That's OK as long as you're refactoring.`
     )
   }
 })
