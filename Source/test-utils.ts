f109217cb0954a018c8c0f55a68f48c962377243
mathieudutour <mathieu@dutour.me>
2018-07-23 09:34:12 -0600

    add some tests for getting symbols

--- Source/test-utils.ts
+++ Source/test-utils.ts
@@ -1,3 +1,23 @@
+const { SymbolMaster, Text, Artboard } = require('./dom')
+
 export function isRunningOnJenkins() {
   return !__command.pluginBundle()
+}
+
+export function createSymbolMaster(document: any) {
+  const artboard = new Artboard({
+    name: 'Test',
+    parent: document.selectedPage,
+  })
+  const text = new Text({
+    text: 'Test value',
+    parent: artboard,
+  })
+
+  // build the symbol master
+  return {
+    master: SymbolMaster.fromArtboard(artboard),
+    text,
+    artboard,
+  }
 }
