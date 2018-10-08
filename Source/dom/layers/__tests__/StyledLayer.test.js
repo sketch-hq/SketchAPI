34a56db1ead5b54eae0d50d2f620c1679bba9b04
mathieudutour <mathieu@dutour.me>
2018-10-08 11:15:15 +0200

    fix setting a style

--- Source/dom/layers/__tests__/StyledLayer.test.js
+++ Source/dom/layers/__tests__/StyledLayer.test.js
@@ -5,3 +5,12 @@ test('should get a style', () => {
   const group = new Shape()
   expect(group.style.type).toBe('Style')
 })
+
+test('should create a Layer with a style property', () => {
+  const group = new Shape({
+    style: {
+      fills: [],
+    },
+  })
+  expect(group.style.type).toBe('Style')
+})
