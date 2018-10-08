d1b257ed5aa6de41d2ae1516b804e7595038aa28
mathieudutour <mathieu@dutour.me>
2018-10-08 15:00:13 +0200

    do bundle core modules

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -5,15 +5,13 @@ const path = require('path')
 const { config: normalConfig, CORE_MODULES } = require('./webpack.config')
 
 module.exports = config => {
-  config.externals = normalConfig.externals.concat(
-    (context, request, callback) => {
-      // sketch API
-      if (/^sketch\//.test(request) || request === 'sketch') {
-        return callback(null, `commonjs ${request}`)
-      }
-      return callback()
+  config.externals = (context, request, callback) => {
+    // sketch API
+    if (/^sketch\//.test(request) || request === 'sketch') {
+      return callback(null, `commonjs ${request}`)
     }
-  )
+    return callback()
+  }
   config.resolve = normalConfig.resolve
   config.resolve.alias = CORE_MODULES.reduce((prev, k) => {
     prev[k] = path.resolve(__dirname, 'core-modules/node_modules/@skpm/', k)
