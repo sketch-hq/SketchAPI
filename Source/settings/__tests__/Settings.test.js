49c99cc8106866aa889a1bf5b258fbc8aa7833c6
mathieudutour <mathieu@dutour.me>
2018-11-28 17:34:22 +0100

    fix setting undefined on layers

--- Source/settings/__tests__/Settings.test.js
+++ Source/settings/__tests__/Settings.test.js
@@ -98,6 +98,23 @@ test('should set a setting on a layer', (context, document) => {
   expect(layerSettingForKey(layer, 'object-key')).toEqual({ a: 1 })
 })
 
+test('should set an undefined setting on a layer', (context, document) => {
+  const layer = new Text({ parent: document.selectedPage })
+  expect(layerSettingForKey(layer, 'object-key')).toBe(undefined)
+
+  setLayerSettingForKey(layer, 'object-key', { a: 1 })
+  expect(layerSettingForKey(layer, 'object-key')).toEqual({ a: 1 })
+
+  setLayerSettingForKey(layer, 'object-key', undefined)
+  expect(layerSettingForKey(layer, 'object-key')).toBe(undefined)
+
+  setLayerSettingForKey(layer, 'object-key', 0)
+  expect(layerSettingForKey(layer, 'object-key')).toBe(0)
+
+  setLayerSettingForKey(layer, 'object-key', null)
+  expect(layerSettingForKey(layer, 'object-key')).toBe(null)
+})
+
 test('should set a setting on a document', (context, document) => {
   setDocumentSettingForKey(document, 'object-key', { a: 1 })
   expect(documentSettingForKey(document, 'object-key')).toEqual({ a: 1 })
