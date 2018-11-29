9cee1f0640d973698820b45cd5b16acffa656407
mathieudutour <mathieu@dutour.me>
2018-11-29 13:43:31 +0100

    update deps

--- Source/dom/layers/__tests__/SymbolMaster.test.js
+++ Source/dom/layers/__tests__/SymbolMaster.test.js
@@ -46,71 +46,68 @@ test('should create a symbol instance from a master', (context, document) => {
   expect(instance.master).toEqual(master)
 })
 
-test(
-  'should create a symbol master with a nested symbol',
-  (context, document) => {
-    // build the first symbol master
-    const { master: nestedMaster, text } = createSymbolMaster(document)
+test('should create a symbol master with a nested symbol', (context, document) => {
+  // build the first symbol master
+  const { master: nestedMaster, text } = createSymbolMaster(document)
 
-    const artboard = new Artboard({
-      name: 'Test2',
-      parent: document.selectedPage,
-    })
-    const text2 = new Text({
-      text: 'Test value 2',
-    })
-    const nestedInstance = nestedMaster.createNewInstance()
-    artboard.layers = [nestedInstance, text2]
+  const artboard = new Artboard({
+    name: 'Test2',
+    parent: document.selectedPage,
+  })
+  const text2 = new Text({
+    text: 'Test value 2',
+  })
+  const nestedInstance = nestedMaster.createNewInstance()
+  artboard.layers = [nestedInstance, text2]
 
-    const master = SymbolMaster.fromArtboard(artboard)
+  const master = SymbolMaster.fromArtboard(artboard)
 
-    const instance = master.createNewInstance()
+  const instance = master.createNewInstance()
 
-    // add the instance to the page
-    document.selectedPage.layers = document.selectedPage.layers.concat(instance)
-    expect(instance.overrides.length).toBe(3)
-    expect(instance.overrides[0].toJSON()).toEqual({
-      type: 'Override',
-      id: `${nestedInstance.id}_symbolID`,
-      path: nestedInstance.id,
-      property: 'symbolID',
-      symbolOverride: true,
-      value: nestedInstance.symbolId,
-      isDefault: true,
-      affectedLayer: {
-        ...nestedInstance.toJSON(),
-        overrides: undefined,
-        selected: undefined,
-        style: instance.overrides[0].affectedLayer.style.toJSON(),
-      },
-    })
-    expect(instance.overrides[1].toJSON()).toEqual({
-      type: 'Override',
-      id: `${text2.id}_stringValue`,
-      path: text2.id,
-      property: 'stringValue',
-      symbolOverride: false,
-      value: 'Test value 2',
-      isDefault: true,
-      affectedLayer: {
-        ...text2.toJSON(),
-        selected: undefined,
-        style: instance.overrides[1].affectedLayer.style.toJSON(),
-      },
-    })
-    expect(instance.overrides[2].toJSON()).toEqual({
-      type: 'Override',
-      id: `${nestedInstance.id}/${text.id}_stringValue`,
-      path: `${nestedInstance.id}/${text.id}`,
-      property: 'stringValue',
-      symbolOverride: false,
-      value: 'Test value',
-      isDefault: true,
-      affectedLayer: {
-        ...text.toJSON(),
-        selected: undefined,
-        style: instance.overrides[2].affectedLayer.style.toJSON(),
-      },
-    })
-  }
-)
+  // add the instance to the page
+  document.selectedPage.layers = document.selectedPage.layers.concat(instance)
+  expect(instance.overrides.length).toBe(3)
+  expect(instance.overrides[0].toJSON()).toEqual({
+    type: 'Override',
+    id: `${nestedInstance.id}_symbolID`,
+    path: nestedInstance.id,
+    property: 'symbolID',
+    symbolOverride: true,
+    value: nestedInstance.symbolId,
+    isDefault: true,
+    affectedLayer: {
+      ...nestedInstance.toJSON(),
+      overrides: undefined,
+      selected: undefined,
+      style: instance.overrides[0].affectedLayer.style.toJSON(),
+    },
+  })
+  expect(instance.overrides[1].toJSON()).toEqual({
+    type: 'Override',
+    id: `${text2.id}_stringValue`,
+    path: text2.id,
+    property: 'stringValue',
+    symbolOverride: false,
+    value: 'Test value 2',
+    isDefault: true,
+    affectedLayer: {
+      ...text2.toJSON(),
+      selected: undefined,
+      style: instance.overrides[1].affectedLayer.style.toJSON(),
+    },
+  })
+  expect(instance.overrides[2].toJSON()).toEqual({
+    type: 'Override',
+    id: `${nestedInstance.id}/${text.id}_stringValue`,
+    path: `${nestedInstance.id}/${text.id}`,
+    property: 'stringValue',
+    symbolOverride: false,
+    value: 'Test value',
+    isDefault: true,
+    affectedLayer: {
+      ...text.toJSON(),
+      selected: undefined,
+      style: instance.overrides[2].affectedLayer.style.toJSON(),
+    },
+  })
+})
