a04a0bf0f4931b103a849c8320c50e18281b789c
mathieudutour <mathieu@dutour.me>
2018-11-29 18:02:27 +0100

    add session variables methods

--- Source/settings/Settings.js
+++ Source/settings/Settings.js
@@ -59,7 +59,7 @@ export function settingForKey(key) {
   const store = NSUserDefaults.alloc().initWithSuiteName(
     `${SUITE_PREFIX}${getPluginIdentifier()}`
   )
-  const value = store.objectForKey_(key)
+  const value = store.objectForKey(key)
 
   if (typeof value === 'undefined' || value == 'undefined' || value === null) {
     return undefined
@@ -134,4 +134,33 @@ export function setDocumentSettingForKey(document, key, value) {
   const documentData = getDocumentData(document)
   const stringifiedValue = JSON.stringify(value, (k, v) => util.toJSObject(v))
   __command.setValue_forKey_onDocument(stringifiedValue, key, documentData)
+}
+
+export function sessionVariable(key) {
+  const threadDic = NSThread.mainThread().threadDictionary()
+
+  const value = threadDic.objectForKey(
+    `${SUITE_PREFIX}${getPluginIdentifier()}.${key}`
+  )
+
+  if (typeof value === 'undefined' || value == 'undefined' || value === null) {
+    return undefined
+  }
+  return JSON.parse(value)
+}
+
+export function setSessionVariable(key, value) {
+  const threadDic = NSThread.mainThread().threadDictionary()
+
+  const stringifiedValue = JSON.stringify(value, (k, v) => util.toJSObject(v))
+  if (!stringifiedValue) {
+    threadDic.removeObjectForKey(
+      `${SUITE_PREFIX}${getPluginIdentifier()}.${key}`
+    )
+  } else {
+    threadDic.setObject_forKey_(
+      stringifiedValue,
+      `${SUITE_PREFIX}${getPluginIdentifier()}.${key}`
+    )
+  }
 }
