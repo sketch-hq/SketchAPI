ee1587ee2f27d7efde3ad810fa2bd859209d8272
mathieudutour <mathieu@dutour.me>
2019-03-15 08:44:43 -0400

    export all the things as default

--- Source/index.ts
+++ Source/index.ts
@@ -1,9 +1,8 @@
-const DOM = require('./dom')
+const completeExport = require('./dom')
+completeExport.Async = require('./async')
+completeExport.DataSupplier = require('./data-supplier')
+completeExport.Settings = require('./settings')
+completeExport.UI = require('./ui')
 
-module.exports = DOM
-module.exports.default = DOM
-
-module.exports.Async = require('./async')
-module.exports.DataSupplier = require('./data-supplier')
-module.exports.Settings = require('./settings')
-module.exports.UI = require('./ui')
+module.exports = completeExport
+module.exports.default = completeExport
