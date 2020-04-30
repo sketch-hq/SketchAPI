465b882e5b5bc2fd78054d9ef5a69caf4a2e5068
Ale M <bomberstudios@gmail.com>
2020-04-30 13:45:13 +0200

    Fixes #722

--- Source/dom/export.js
+++ Source/dom/export.js
@@ -233,12 +233,14 @@ export function exportObject(object, options) {
 export function objectFromJSON(sketchJSON, version) {
   const v = version || MSArchiveHeader.metadataForNewHeader().version
   const ptr = MOPointer.new()
-  let object = MSJSONDictionaryUnarchiver.unarchiveObjectFromDictionary_asVersion_corruptionDetected_error(
-    sketchJSON,
-    v,
-    null,
-    ptr
-  )
+  const sketchVersion = MSApplicationMetadata.metadata().appVersion
+  let method
+  if (sketchVersion < 64) {
+    method = 'unarchiveObjectFromDictionary_asVersion_corruptionDetected_error'
+  } else {
+    method = 'unarchivedObjectFromDictionary_asVersion_corruptionDetected_error'
+  }
+  let object = MSJSONDictionaryUnarchiver[method](sketchJSON, v, null, ptr)
   if (ptr.value()) {
     throw new Error(`Failed to create object from sketch JSON: ${ptr.value()}`)
   }
