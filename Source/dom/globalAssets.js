74aeded1ba1bea40dded828467c2810761387863
Ale M <bomberstudios@gmail.com>
2020-09-16 10:46:37 +0200

    Turns out we're not exposing swatches globally, since they'll live inside a Library

--- Source/dom/globalAssets.js
+++ Source/dom/globalAssets.js
@@ -1,5 +1,5 @@
 import { toArray } from 'util'
-import { ColorAsset, GradientAsset, Swatch } from './assets'
+import { ColorAsset, GradientAsset } from './assets'
 import { hookedArray } from './utils'
 
 export const globalAssets = {}
@@ -84,34 +84,3 @@ export function getGlobalGradients() {
     GradientAsset.fromNative(a)
   )
 }
-
-/**
- * Access any global swatches
- *
- * @return {Array<Swatch>} A list of swatches defined globally
- */
-const globalSwatchesDescriptor = {
-  get() {
-    const arr = toArray(nativeAssets.colorAssets()).map((a) =>
-      Swatch.fromNative(a)
-    )
-    return hookedArray(arr, null, globalSwatchesDescriptor)
-  },
-  set(swatches) {
-    nativeAssets.removeAllColorAssets()
-    toArray(swatches)
-      .map((c) => ColorAsset.from(c))
-      .forEach((c) => {
-        nativeAssets.addColorAsset(c._object)
-      })
-  },
-  insertItem(color, index) {
-    const wrapped = ColorAsset.from(color)
-    nativeAssets.insertColorAsset_atIndex(wrapped._object, index)
-    return wrapped
-  },
-  removeItem(index) {
-    return nativeAssets.removeColorAssetAtIndex(index)
-  },
-}
-Object.defineProperty(globalAssets, 'swatches', globalSwatchesDescriptor)
