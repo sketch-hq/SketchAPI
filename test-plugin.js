59468530f05f53a588c2c22c57db90a98dff4d83
Christian Klotz <hello@christianklotz.co.uk>
2020-11-06 12:05:19 +0000

    Use webpack to build plugin
    Run `npm webpack ./test-plugin.js`

--- test-plugin.js
+++ test-plugin.js
@@ -3,11 +3,12 @@ const os = require('os')
 const path = require('path')
 const globby = require('globby')
 
-const webpack = require('webpack')
+const VirtualModulesPlugin = require('webpack-virtual-modules')
+const CopyPlugin = require('copy-webpack-plugin')
 
 // TODO:
-// - scan source folder recursively
-// - skip if ignored
+// ✔ scan source folder recursively
+// ✔ skip if ignored
 // - export default module containing test suites, bundle all into single script
 // - load output file
 // - parse results
@@ -15,37 +16,13 @@ const webpack = require('webpack')
 // - use UUID for plugin name and identifier in manifest
 // - use argument as output file path (include in script)
 // - delete plugin after run
+// - Run plugin within Sketch using `open`
 
 const plugin = path.join(
   os.homedir(),
   'Library/Application Support/com.bohemiancoding.sketch3/Plugins/SketchIntegrationTests.sketchplugin',
   'Contents/Sketch'
 )
-
-if (!fs.existsSync(plugin)) {
-  fs.mkdirSync(plugin, { recursive: true })
-}
-
-const manifest = {
-  commands: [
-    {
-      name: 'Test Sketch API',
-      identifier: 'test',
-      script: 'tests.js',
-      scope: 'application',
-      handlers: {
-        run: 'onRun',
-        actions: {
-          HandleURL: 'onRun',
-        },
-      },
-    },
-  ],
-  version: '1.0.0',
-  name: 'Sketch API Integration Tests',
-  identifier: 'com.sketch.plugin.integration-tests',
-  disableCocoaScriptPreprocessor: true,
-}
 
 const script = `
   function onRun(context) {
@@ -60,11 +37,13 @@ const script = `
     data.writeToFile_atomically_encoding_error(out, false, NSUTF8StringEncoding, err)
   }
 `
-fs.writeFileSync(path.join(plugin, 'manifest.json'), JSON.stringify(manifest))
-// fs.writeFileSync(path.join(plugin, 'tests.js'), script)
 
-// Run plugin within Sketch using `open`
-
+/**
+ * Walks a directory and returns a generator for all files within the
+ * directory and all included subdirectories.
+ *
+ * @param {string} dir A path to a directory to be read recursively.
+ */
 async function* walk(dir) {
   for await (const d of await fs.promises.opendir(dir)) {
     const entry = path.join(dir, d.name)
@@ -73,11 +52,36 @@ async function* walk(dir) {
   }
 }
 
-async function main() {
+/**
+ * Converts package.json into a plugin manifest.
+ */
+function manifest(pkg) {
+  return JSON.stringify({
+    commands: [
+      {
+        name: 'Test Sketch API',
+        identifier: 'test',
+        script: 'tests.js',
+        scope: 'application',
+        handlers: {
+          run: 'onRun',
+          actions: {
+            HandleURL: 'onRun',
+          },
+        },
+      },
+    ],
+    version: pkg.version,
+    name: 'Sketch API Integration Tests',
+    identifier: 'com.sketch.plugin.integration-tests',
+    disableCocoaScriptPreprocessor: true,
+  })
+}
+
+async function testSuites(dir) {
   const isIgnored = await globby.gitignore()
   const isTest = /(.*\/)*__tests__\/(.*)\.test\.js/i
 
-  let dir = process.cwd()
   let all = []
   for await (const p of walk(dir)) {
     if (isIgnored(p)) continue
@@ -85,22 +89,41 @@ async function main() {
 
     all.push({ name: p.replace(isTest, '$2'), path: p })
   }
-
-  let config = {
-    output: {
-      filename: 'tests.js',
-      path: plugin,
-    },
-    entry: path.join(plugin, 'source.js'),
-  }
-
-  webpack(config)
+  return all
 }
 
 function source(tests) {
-  return tests.reduce(
-    (prev, curr) => `${prev}\nimport ${curr.name} from '${curr.path}'`
-  )
+  const reducer = (prev, curr) =>
+    `${prev}\nimport ${curr.name} from '${curr.path}'`
+  return tests.reduce(reducer, '')
 }
 
-main()
+const { NODE_ENV } = process.env
+
+module.exports = {
+  mode: NODE_ENV || 'development',
+  output: {
+    filename: 'tests.js',
+    path: plugin,
+  },
+  entry: {
+    index: './tests.js',
+  },
+  plugins: [
+    new VirtualModulesPlugin({
+      './tests.js': testSuites(process.cwd()).then((tests) => source(tests)),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './package.json',
+          to: `${plugin}/manifest.json`,
+          transform(content) {
+            const pkg = JSON.parse(content.toString())
+            return manifest(pkg)
+          },
+        },
+      ],
+    }),
+  ],
+}
