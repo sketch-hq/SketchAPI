82d4bdc7bef28f3dba23cdfbe0fe27ee37deea23
Rúben Almeida <almeidarruben@gmail.com>
2021-02-16 17:34:24 -0100

    Parse and analyse test results output file - parse test results output file - pretty print test results - return proper exit code

--- test.py
+++ test.py
@@ -3,7 +3,112 @@ import sys
 import getopt
 import os
 import json
+import tempfile
+import time
 from pathlib import Path, PurePath
+
+
+def group_results_by_parent(results):
+    grouped_results = {}
+
+    for result in results:
+        parent_title = result['ancestorTitles'][0]
+        if (parent_title in grouped_results):
+            parent = grouped_results[result['ancestorTitles'][0]]
+            parent['results'].append({
+                'ancestorTitles': result['ancestorTitles'][1:],
+                'title': result['title'],
+                'status': result['status']
+            })
+        else:
+            grouped_results[parent_title] = {
+                'results': [
+                    {
+                        'ancestorTitles': result['ancestorTitles'][1:],
+                        'title': result['title'],
+                        'status': result['status'],
+                    },
+                ]
+            }
+
+    return grouped_results
+
+
+def colored_status_text(status, status_text):
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    RESET = "\033[0;0m"
+
+    status_color = GREEN if status == 'passed' else RED
+    return status_color + status_text + RESET
+
+
+def has_failed_tests(results):
+    hasFailedTests = False
+    
+    for result in results:
+        if result['status'] == 'failed':
+            hasFailedTests = True
+            break
+
+    return hasFailedTests
+
+
+def print_results(results):
+    for parent_name in results:
+        test_results = results[parent_name]["results"]
+
+        global_status = 'failed' if has_failed_tests(test_results) else 'passed'
+        print('\n{status} {name}'.format(
+            status=colored_status_text(global_status, global_status.upper()),
+            name=parent_name
+        ))
+
+        for result in test_results:
+            ancestorTitles = result['ancestorTitles']
+            ancestors = ''
+
+            if (len(ancestorTitles)):
+                for ancestor in result['ancestorTitles']:
+                    ancestors += ' → {}'.format(ancestor)
+
+            status_text = '✔' if result['status'] == 'passed' else '𐄂'
+            print('{status} {ancestors} {title}'.format(
+                status=colored_status_text(result['status'], status_text),
+                ancestors=ancestors,
+                title=result['title']
+            ))
+
+
+def parse_test_results():
+    output_file_path = PurePath(
+        tempfile.gettempdir(), 'SketchIntegationTests-output.log')
+
+    # TODO: Find a better approach for waiting to output file to be available
+    while not os.path.exists(output_file_path):
+        time.sleep(2)
+
+    if os.path.isfile(output_file_path):
+        with open(output_file_path, 'r') as f:
+            data = f.read()
+            json_data = json.loads(data)
+
+            grouped_results = group_results_by_parent(json_data)
+            print_results(grouped_results)
+
+            if (has_failed_tests(json_data)):
+                RED = '\033[91m'
+                print(RED + 'Failed with exit code 1')
+                sys.exit(1)
+            else:
+                GREEN = '\033[92m'
+                print(GREEN + 'All test suites passed.')
+                sys.exit(0)
+            
+            # remove test output file
+            os.remove(output_file_path)
+    else:
+        raise 'File not found'
 
 
 def main(argv):
@@ -17,6 +122,7 @@ def main(argv):
     except getopt.GetoptError:
         print('test.py -s <sketch> -p <plugin>')
         sys.exit(2)
+
     for opt, arg in opts:
         if opt == '-h':
             print('test.py -s <sketch> -p <plugin>')
@@ -29,15 +135,18 @@ def main(argv):
     if not plugin:
         print('test.py -s <sketch> -p <plugin>')
 
-    print('Plugin: {}'.format(plugin))
     # create a symbolic link to the plugin because Sketch expects it to
     # be inside the Application Support Plugins folder.
-    path = PurePath(
+    plugin_path = PurePath(
         str(Path.home()),
         "Library/Application Support/com.bohemiancoding.sketch3/Plugins",
         os.path.basename(plugin))
 
-    # os.symlink(os.path.abspath(plugin), path, target_is_directory=True)
+    # FIXME: not sure if we need this in prod
+    if (os.path.exists(plugin_path)):
+        os.remove(plugin_path)
+
+    os.symlink(os.path.abspath(plugin), plugin_path, target_is_directory=True)
 
     # get the plugin identifier from the manifest, Sketch uses the
     # identifier to look up the corresponding plugin.
@@ -45,13 +154,16 @@ def main(argv):
         data = f.read()
 
     manifest = json.loads(data)
-    # subprocess.call([
-    #     "open", "-na", sketch,
-    #     "sketch://plugin/{}/test".format(manifest['identifier']),
-    #     ])
+    sketch_process = subprocess.Popen([
+        "open", "-a", sketch,
+        "sketch://plugin/{}/test".format(manifest['identifier']),
+    ])
+    sketch_process.wait()
+
+    parse_test_results()
 
     # cleanup and delete the symbolic link
-    # os.remove(path)
+    os.remove(plugin_path)
 
 
 if __name__ == "__main__":
