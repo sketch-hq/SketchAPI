363ff2dc0d3c49809a5363c99129ea97fcbc2ea4
Rúben Almeida <almeidarruben@gmail.com>
2021-02-18 16:25:46 -0100

    Use relative path instead of temp directory for output file

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
 # TODO: Variables are set on CI??
 export IDENTIFIER=xpto
-export OUTPUT_FILE_NAME=SketchIntegrationTests-output.log
+export OUTPUT_FILE_PATH=SketchIntegrationTests-output.log
 
 npm run test:build \
 --identifier=${IDENTIFIER} \
---output=${OUTPUT_FILE_NAME} && \
+--output=${OUTPUT_FILE_PATH} && \
 
 python3 test.py \
 -s /Applications/Sketch.app \
 -p build/SketchIntegrationTests-xpto.sketchplugin \
--o ${OUTPUT_FILE_NAME}
+-o ${OUTPUT_FILE_PATH}
